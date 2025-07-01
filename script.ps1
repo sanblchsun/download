888160d7b387fa99cce16a34665342b1ae0d8ab8
Александр <logovopost@yandex.ru>
2025-07-01 12:25:53 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,1 +1,25 @@
-Enable-PSRemoting
+# Получаем информацию о текущем пользователе
+$currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+$isAdmin = [Security.Principal.WindowsPrincipal]::new($currentUser).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if ($isAdmin) {
+    # Задаем имя и пароль для нового пользователя
+    $username = "admin1"
+    $password = "123" | ConvertTo-SecureString -AsPlainText -Force
+    
+    # Проверяем, существует ли пользователь с таким именем
+    if (-not (Get-LocalUser -Name $username -ErrorAction SilentlyContinue)) {
+        # Создаем нового пользователя
+        New-LocalUser -Name $username -Password $password -FullName "Administrator User" -Description "New admin account"
+        
+        # Добавляем нового пользователя в группу администраторов
+        Add-LocalGroupMember -Group "Administrators" -Member $username
+        Write-Host "Пользователь $username создан и добавлен в группу администраторов."
+    } else {
+        Write-Host "Пользователь с именем $username уже существует."
+    }
+} else {
+    Write-Host "Текущему пользователю $($currentUser.Name) не хватает прав администратора."
+}
+
+Pause
